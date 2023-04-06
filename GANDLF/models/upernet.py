fd2f6ac5df0581a14cf9088cf0c41626380942cf
sarthakpati <sarthak.pati@hotmail.com>
2023-04-06 13:45:10 -0400

    ensure number of labels and channels are getting correctly assigned

--- GANDLF/models/upernet.py
+++ GANDLF/models/upernet.py
@@ -13,26 +13,47 @@ class UPerNet(ModelBase):
     ):
         super(UPerNet, self).__init__(parameters)
         # Initializing a model (with random weights) from the convnext-tiny-224 style configuration
-        self.model = UperNetForSemanticSegmentation.from_pretrained(
+        self.model_2d = UperNetForSemanticSegmentation.from_pretrained(
             "openmmlab/upernet-convnext-tiny"
         )
 
         if self.n_dimensions == 3:
-            self.model = self.converter(self.model)
+            self.model = self.converter(self.model_2d)
+        else:
+            self.model = self.model_2d
+
+        # set the number of classes and channels
+        self.model.decode_head.classifier.out_channels = self.n_classes
+        self.model.backbone.embeddings.patch_embeddings.in_channels = self.n_channels
+
+        self.model.config.__setattr__("num_labels", self.n_classes)
+        backbone_config = self.model.config.__getattribute__("backbone_config")
+        backbone_config.__setattr__("num_channels", self.n_channels)
+        self.model.config.__setattr__("backbone_config", backbone_config)
 
     def forward(self, x):
-        original_shape = x.shape
-        size_to_transform = self.model.config.__getattribute__(
-            "backbone_config"
-        ).__getattribute__("image_size")
+        # original_shape = x.shape
+        # size_to_transform = self.model.config.__getattribute__(
+        #     "backbone_config"
+        # ).__getattribute__("image_size")
 
-        x_transformed = CropOrPad(
-            (original_shape[1], size_to_transform, size_to_transform)
-        )(x)
+        # size_to_transform = self.model.config.__getattribute__(
+        #     "hidden_size"
+        # )
 
-        output_from_model = self.model(pixel_values=x_transformed)
-        output_original_shape = CropOrPad(tuple(original_shape[1:]))(
-            output_from_model.logits
-        )
+        # if len(original_shape) == 5:
+        #     x_transformed = CropOrPad(
+        #         (original_shape[1], size_to_transform, size_to_transform, size_to_transform)
+        #     )(x)
+        # else:
+        #     x_transformed = CropOrPad(
+        #         (original_shape[1], size_to_transform, size_to_transform)
+        #     )(x)
 
-        return self.apply_final_layer(output_original_shape)
+        output_from_model = self.model(x)
+
+        # output_original_shape = CropOrPad(tuple(original_shape[1:]))(
+        #     output_from_model.logits
+        # )
+
+        return self.apply_final_layer(output_from_model)
