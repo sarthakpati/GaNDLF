788c2b4aca7497735a3801bfc1dc4716ca158521
sarthakpati <sarthak.pati@hotmail.com>
2023-04-06 12:01:07 -0400

    simpler configuration, and updated with crop/pad transforms

--- GANDLF/models/upernet.py
+++ GANDLF/models/upernet.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+
 from .modelBase import ModelBase
-from transformers import ConvNextConfig, UperNetConfig, UperNetForSemanticSegmentation
+from transformers import UperNetForSemanticSegmentation
+from torchio.transforms import CropOrPad
 
 
 class UPerNet(ModelBase):
@@ -10,76 +12,27 @@ class UPerNet(ModelBase):
         parameters: dict,
     ):
         super(UPerNet, self).__init__(parameters)
-        # Initializing a ConvNext convnext-tiny-224 style configuration
-        backbone_config = ConvNextConfig()
-        """ default config
-        ConvNextConfig {
-            "depths": [
-                3,
-                3,
-                9,
-                3
-            ],
-            "drop_path_rate": 0.0,
-            "hidden_act": "gelu",
-            "hidden_sizes": [
-                96,
-                192,
-                384,
-                768
-            ],
-            "image_size": 224,
-            "initializer_range": 0.02,
-            "layer_norm_eps": 1e-12,
-            "layer_scale_init_value": 1e-06,
-            "model_type": "convnext",
-            "num_channels": 3,
-            "num_stages": 4,
-            "out_features": null,
-            "patch_size": 4,
-            "stage_names": [
-                "stem",
-                "stage1",
-                "stage2",
-                "stage3",
-                "stage4"
-            ],
-            "transformers_version": "4.26.1"
-        }
-        """
-
-        # translate parameters to ConvNextConfig
-        backbone_config.__setattr__("depths", parameters["model"].get("depths", [3, 3]))
-        backbone_config.__setattr__(
-            "hidden_sizes", parameters["model"].get("hidden_sizes", [96, 192])
-        )
-        backbone_config.__setattr__(
-            "num_stages", parameters["model"].get("num_stages", 2)
-        )
-        backbone_config.__setattr__(
-            "stage_names", parameters["model"].get("stage_names", ["stem", "stage1"])
-        )
-
-        backbone_config = ConvNextConfig(out_features=["stage1", "stage2", "stage3", "stage4"])
-        config = UperNetConfig(backbone_config=backbone_config)
-
         # Initializing a model (with random weights) from the convnext-tiny-224 style configuration
-        model = UperNetForSemanticSegmentation(config)
+        self.model = UperNetForSemanticSegmentation.from_pretrained(
+            "openmmlab/upernet-convnext-tiny"
+        )
 
         if self.n_dimensions == 3:
             self.model = self.converter(self.model)
 
     def forward(self, x):
-        output_from_convnext = self.model(pixel_values=x)
-        # output_from_convnext.keys()
-        # odict_keys(['last_hidden_state', 'pooler_output'])
-        # none of these are corresponding to the size of the input image, which is [128,128]
+        original_shape = x.shape
+        size_to_transform = self.model.config.__getattribute__(
+            "backbone_config"
+        ).__getattribute__("image_size")
 
-        pooler_output_after_final_layer = self.final_convolution_layer(
-            output_from_convnext.pooler_output
-        )
-        last_hidden_state_after_final_layer = self.final_convolution_layer(
-            output_from_convnext.last_hidden_state
+        x_transformed = CropOrPad(
+            (original_shape[1], size_to_transform, size_to_transform)
+        )(x)
+
+        output_from_model = self.model(pixel_values=x_transformed)
+        output_original_shape = CropOrPad(tuple(original_shape[1:]))(
+            output_from_model.logits
         )
 
-        return output_from_convnext.pooler_output
+        return self.apply_final_layer(output_original_shape)
