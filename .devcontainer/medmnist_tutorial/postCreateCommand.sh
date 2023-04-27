838cba0fa4dd3f05e8b55531f100ce497072a5d2
Sarthak Pati <sarthak.pati@pennmedicine.upenn.edu>
2023-04-27 11:37:41 -0400

    Create postCreateCommand.sh

---
+++ .devcontainer/medmnist_tutorial/postCreateCommand.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# if there is a gpu present in the container, install the gpu version of pytorch
+# we will check for the presence of the nvidia-smi command as that signifies the presence of a gpu
+
+# if runnning on a GPU machine, install the GPU version of pytorch
+if command -v nvidia-smi &> /dev/null
+then
+	pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+fi
+
+# this is going to be specific for anyone who wants to use medmnist
+python -m medmnist download
+python -m medmnist save --flag=pathmnist --folder=medmnist/dataset/ --postfix=png
+
+pip install -e .
+python ./gandlf_verifyInstall
+gzip -dk -r tutorial/medmnist/dataset
